86cf5fd0d37a3cae5f9c225910f77587f0d57cde
upgradingdave <upgradingdave@gmail.com>
2019-04-25 06:47:05 -0400

    Able to send message using kitteh library

--- src/clj/upgrade/core.clj
+++ src/clj/upgrade/core.clj
@@ -1,6 +1,9 @@
 (ns upgrade.core
-  (:import [org.pircbotx Configuration Configuration$Builder PircBotX]
-           [upgrade.encrypt EncryptionManager]))
+  (:import [org.kitteh.irc.client.library Client]
+           [org.kitteh.irc.client.library.event.channel ChannelJoinEvent]
+           [org.kitteh.irc.client.library.feature.twitch TwitchSupport]
+           [upgrade.encrypt EncryptionManager]
+           ))
 
 (defn decrypt [message]
   (.. (EncryptionManager.) (decrypt (java.io.File. "./mykeyfile") message)))
@@ -10,29 +13,30 @@
    :oauth (decrypt "AAAADPPnM4SgS6br8YQs2T4zGRzNChlE+cN2uoYhmu465YwIS0V80NO0Si4Cst2sYHoijSpVT2ymyisZptKuuBAf+Xs=")
    :channel "upgradingdave"
    :server "irc.chat.twitch.tv"
-   :port 6667
+   :port 443
    })
 
-(defn create-listener []
-  (proxy [org.pircbotx.hooks.ListenerAdapter] []
-    (onPing [event] (println (. event (getPingValue))))))
+(defn create-client [conf]
+  (let [client (.. (Client/builder)
+                   (server)
+                   (host (:server conf))
+                   (port (:port conf))
+                   (password (:oauth conf))
+                   (then)
+                   (nick (:username conf))
+                   (build))]
 
-(defn create-irc-client [conf]
-  (let [config (.. (Configuration$Builder.)
-                   (setName (:username conf))
-                   (setServer (:server conf) (:port conf))
-                   (setServerPassword (:oauth conf))
-                   (addAutoJoinChannel (str "#" (:channel conf)))
-                   (addListener (create-listener))
-                   (buildConfiguration))]
-    (PircBotX. config)))
-
-
+    (-> client (TwitchSupport/addSupport)
+        (. (connect)))
+    client))
 
 (defn -main []
   (println "Attempting to start twitch chat bot ... ")
-  (let [client (create-irc-client (get-config))]
-    (.startBot client)))
+  (let [conf (get-config)
+        channel (str "#" (:channel conf))
+        client (create-client conf)]
+    (. client (addChannel (into-array String [channel])))
+    (. client (sendMessage channel "Upgradingdave's Bot is ALIVE!"))))
 
 
 
